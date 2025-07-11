77cba0750b10a52295e723b3b1344f4d516f1da0
altmash.m2024@gmail.com <altmash.m2024@gmail.com>
2025-07-11 16:39:20 +0530

    push anything

--- src/main/java/com/example/DemoactionApplication.java
+++ src/main/java/com/example/DemoactionApplication.java
@@ -8,6 +8,9 @@ public class DemoactionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoactionApplication.class, args);
-	}
 
+
+		System.out.println("Hello, World! This is a simple Spring Boot application.");
+
+	}
 }
